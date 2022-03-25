8567f0de4ea7580ced758c0df8b5149c74c62f6c
your_github_username <your_github_email>
2022-03-25 18:23:06 +0530

    Day21

---
+++ javaScript/Day21/index3.js
@@ -0,0 +1,76 @@
+// Data Type in JS
+// var number1 = 22;
+// var number2 = 52;
+//String dataType
+// var name1 = "PIYUSH";
+// var name2 = 'NIKHIL';
+
+//Objects
+// var marks = {
+//     Piyush: 44,
+//     Ravi: 33,
+//     Harry: 55
+//}
+// console.log(number1);
+// console.log(number2);
+// console.log(name1);
+// console.log(marks);
+
+//var und;
+//console.log(und);
+
+//var n = null;
+//console.log(n);
+//arr = [22, 44, 65, 7];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+//Logical not
+//console.log(!false);
+//console.log(!true);
+
+//If-else Ladder
+// var age = 52;
+// if (age > 32) {
+//     console.log("you are not a kid");
+// }
+// else if (age < 32) {
+//     console.log("32 you are a boy")
+// }
+// else if (age < 25) {
+//     console.log("your age is smaller then 25")
+// }
+// else if (age < 18) {
+//     console.log("you are smaller them 18")
+// }
+// else {
+//     console.log("you are just a kid")
+// }
+
+// arr = [22,89,5,66];
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+
+// }
+
+// arr.forEach(function(element){
+//     console.log(element);
+
+// });
+//arr = [22, 89, 5, 66];
+// let j = 0;
+// while (j<arr.length) {
+//     console.log(arr[j])
+//     j++;
+//}
+arr = [22, 89, 5, 66];
+let j = 0;
+do {
+    console.log(arr[j]);
+    j++
+
+} while (j < arr.length) {
+    console.log([i])
+
+}
